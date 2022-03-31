2ed89fd01e26951e7f76567edbee49453f170a53
mayssa <mayssouch1234@gmail.com>
2022-03-31 17:25:57 +0200

    ma deeuxieme commit

---
+++ lte-react/src/App.js
@@ -0,0 +1,28 @@
+//import logo from './logo.svg';
+//import './App.css';
+//import les commponents 
+import Header from "./components/Header";
+
+import Footer from "./components/Footer";
+import SideNav from "./components/SideNav";
+import Home from "./components/Home";
+
+
+
+function App() {
+  return (
+    <>
+     <Header />
+   <Home /> 
+     <SideNav />
+     <Footer />
+
+
+
+    
+
+    </>
+  );
+}
+
+export default App;
